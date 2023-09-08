6cbe4cf1ed78ea5c46c4249c88fb6aad81a68f8f
Theads1 <tiaradheads@gmail.com>
2023-09-08 12:47:43 -0400

    tweaked login and register pages

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import { Fragment } from "react"
+import "./login.css"
+import Navbar from "../homepage/Navbar"
+
 
 const Login = () => {
   return (
-    <div>
-      
+    <Fragment>
+    <div className="login">
+    <div className="logoContainer">
+        <Navbar/>
     </div>
+ <div className="loginContainer">
+    <div className="logTitle">Login </div>
+    <form className="form">
+      <p className="message">
+      Happy Shopping 😃{" "}
+      </p>
+
+      <label>
+        <input className="input" type="email" placeholder="" required="" />
+        <span>Email</span>
+      </label>
+
+      <label>
+        <input
+          className="input"
+          type="password"
+          placeholder=""
+          required=""
+        />
+        <span>Password</span>
+      </label>
+      <button className="submit">Log In</button>
+      <a href="#">forgot password?</a>{" "}
+
+      <p className="signin">
+        Don't have an account ? <a href="#">Create an account</a>{" "}
+      </p>
+    </form>
+  </div>
+</div>
+</Fragment>
   )
 }
 
--- client/src/components/user/Register.jsx
+++ client/src/components/user/Register.jsx
@@ -1,10 +1,67 @@
-
+import { Fragment } from "react"
+import "./register.css"
+import Navbar from "../homepage/Navbar"
 const Register = () => {
   return (
-    <div>
-      
+    <Fragment>
+        <div className="register">
+        <div className="logoContainer">
+            <Navbar/>
+        </div>
+     <div className="registerContainer">
+        <div className="regTitle">Register Here</div>
+        <form className="form">
+          <p className="message">
+            Signup now for access to exclusive savings.{" "}
+          </p>
+          <div className="flex">
+            <label>
+              <input className="input" type="text" placeholder="" required="" />
+              <span>Firstname</span>
+            </label>
+
+            <label>
+              <input className="input" type="text" placeholder="" required="" />
+              <span>Lastname</span>
+            </label>
+          </div>
+
+          <label>
+            <input className="input" type="email" placeholder="" required="" />
+            <span>Email</span>
+          </label>
+
+          <label>
+            <input
+              className="input"
+              type="password"
+              placeholder=""
+              required=""
+            />
+            <span>Password</span>
+          </label>
+          <label>
+            <input
+              className="input"
+              type="password"
+              placeholder=""
+              required=""
+            />
+            <span>Confirm password</span>
+          </label>
+          <p className="agreement">
+            Creating an account means you agree to our Terms and Conditions 😃
+          </p>
+          <button className="submit">Create Account</button>
+          <p className="signin">
+            Already have an acount ? <a href="#">Signin</a>{" "}
+          </p>
+        </form>
+      </div>
     </div>
-  )
+    </Fragment>
+
+  );
 }
 
-export default Register
+export default Register;
