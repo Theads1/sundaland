16f8af7a4388cab5642d4eaf76d8facc52c5043b
Theads1 <tiaradheads@gmail.com>
2023-09-07 22:03:10 -0400

    removed component info

---
+++ client/src/pages/Products.jsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/homepage/Navbar'
+import Merch from '../components/products/Merch'
+import Sidebar from '../components/products/Sidebar'
+
+const Products = () => {
+  return (
+     <div className='navbarcontainer'>
+        <Navbar/>
+        <Merch/>
+        <Sidebar/>
+        </div>
+
+       
+  )
+}
+
+export default Products
