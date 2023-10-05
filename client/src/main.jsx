7da774fab0c2c65e3acb206d866a361320117548
Theads1 <tiaradheads@gmail.com>
2023-10-05 18:09:00 -0400

    env testing for server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import {Provider} from "react-redux"
 import store from './redux/store.js'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
    <Provider store={store}>
     <App />
